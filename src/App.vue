8437a460c3f8a6b5bf596ce5857d0322802cbc20
atcha <francois.cornuaud@gmail.com>
2020-03-18 10:26:49 +0100

    Add validation

--- src/App.vue
+++ src/App.vue
@@ -3,56 +3,68 @@
         <h1 class="text-2xl mb-4">Attestation de déplacement dérogatoire</h1>
         <h2 class="mb-4">En application de l'article 1er du décret du 16 mars 2020 portant réglementation des
             déplacements dans le cadre de la lutte contre la propagation du virus Covid-19 :</h2>
-        <h3 class="mb-4">Je soussigné(e)</h3>
-        <div class="flex flex-col mb-4">
-            <div class="flex flex-col justify-between mb-4">
-                <label for="name">Mme / M.</label>
-                <input id="name" name="name" type="text" v-model="name">
-            </div>
-            <div class="flex flex-col justify-between mb-4">
-                <label for="birthdate">Né(e) le :</label>
-                <input id="birthdate" name="birthdate" type="date" v-model="birthDate">
-            </div>
-            <div class="flex flex-col justify-between mb-4">
-                <label for="adress">Demeurant</label>
-                <input id="adress" name="adress" type="text" v-model="address">
-            </div>
-        </div>
-        <h3 class="mb-4">certifie que mon déplacement est lié au motif suivant (cocher la case) autorisé par l'article
-            1er du décret
-            du 16 mars 2020 portant réglementation des déplacements dans le cadre de la lutte contre la propagation du
-            virus Covid-19:</h3>
-        <div class="mb-4">
-            <p class="mb-2" v-for="value in choiceValues" :key="value.id">
-                <input class="mr-2" :id="'choice_' + value.id" type="radio" name="choice" :value="value.text" v-model="choice">
-                <label :for="'choice_' + value.id">{{value.text}}</label>
-            </p>
-        </div>
-        <div>
+        <form @submit.prevent="submitForm">
+            <h3 class="mb-4">Je soussigné(e)</h3>
             <div class="flex flex-col mb-4">
                 <div class="flex flex-col justify-between mb-4">
-                    <label for="place">Fait à</label> <input id="place" name="place" type="text" v-model="place">
+                    <label for="name">Mme / M.</label>
+                    <input id="name" name="name" type="text" v-model="name" :class="{ 'is-invalid': $v.name.$error }">
+                    <div class="error" v-if="submitted && !$v.name.required">Le nom est requis</div>
                 </div>
                 <div class="flex flex-col justify-between mb-4">
-                    <label for="date">le</label> <input id="date" name="date" type="date" v-model="date">
+                    <label for="birthdate">Né(e) le :</label>
+                    <input id="birthdate" name="birthdate" type="date" v-model="birthDate" :class="{ 'is-invalid': $v.birthDate.$error }">
+                    <div class="error" v-if="submitted && !$v.birthDate.required">La date de naissance est requise</div>
+                </div>
+                <div class="flex flex-col justify-between mb-4">
+                    <label for="adress">Demeurant</label>
+                    <input id="adress" name="adress" type="text" v-model="address" :class="{ 'is-invalid': $v.address.$error }">
+                    <div class="error" v-if="submitted && !$v.address.required">L'adresse est requise</div>
                 </div>
             </div>
-        </div>
-        <div>
-            <h4 class="text-gray-700 font-bold mb-4">Signature</h4>
-            <drawing-board ref="drawingBoard"></drawing-board>
-            <button class="inline-block btn mt-4" @click="$refs.drawingBoard.clear()">Effacer signature</button>
-        </div>
-        <div class="flex justify-between mt-6">
-            <button class="btn" @click="createPdf">Générer PDF</button>
-            <button class="btn" @click="resetForm">Reset Formulaire</button>
-        </div>
+            <h3 class="mb-4">certifie que mon déplacement est lié au motif suivant (cocher la case) autorisé par
+                l'article
+                1er du décret
+                du 16 mars 2020 portant réglementation des déplacements dans le cadre de la lutte contre la propagation
+                du
+                virus Covid-19:</h3>
+            <div class="mb-4">
+                <p class="mb-2" v-for="value in choiceValues" :key="value.id">
+                    <input class="mr-2" :id="'choice_' + value.id" type="radio" name="choice" :value="value.text"
+                           v-model="choice" :class="{ 'is-invalid': $v.choice.$error }">
+                    <label :for="'choice_' + value.id" :class="{ 'text-red-700': $v.choice.$error }">{{value.text}}</label>
+                </p>
+                <div class="error" v-if="submitted && !$v.choice.required">Le choix de sortie est requis</div>
+            </div>
+            <div>
+                <div class="flex flex-col mb-4">
+                    <div class="flex flex-col justify-between mb-4">
+                        <label for="place">Fait à</label> <input id="place" name="place" type="text" v-model="place" :class="{ 'is-invalid': $v.place.$error }">
+                        <div class="error" v-if="submitted && !$v.place.required">L'endroit de réalisation est requis</div>
+                    </div>
+                    <div class="flex flex-col justify-between mb-4">
+                        <label for="date">le</label> <input id="date" name="date" type="date" v-model="date" :class="{ 'is-invalid': $v.date.$error }">
+                        <div class="error" v-if="submitted && !$v.date.required">La date de réalisation est requise</div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h4 class="text-gray-700 font-bold mb-4">Signature</h4>
+                <drawing-board ref="drawingBoard"></drawing-board>
+                <button class="inline-block btn mt-4" @click="$refs.drawingBoard.clear()">Effacer signature</button>
+            </div>
+            <div class="flex justify-between mt-6">
+                <button class="btn" type="submit">Générer PDF</button>
+                <button class="btn" @click="resetForm">Reset Formulaire</button>
+            </div>
+        </form>
     </div>
 </template>
 
 <script>
     import DrawingBoard from "./components/DrawingBoard";
     import jsPdf from 'jspdf';
+    import {required} from 'vuelidate/lib/validators'
 
     export default {
         name: 'App',
@@ -87,8 +99,17 @@
                         id: 3,
                         text: "déplacements brefs, à proximité du domicile, liés à l'activité physique individuelle des personnes, à l'exclusion de toute pratique sportive collective, et aux besoins des animaux de compagnie."
                     }
-                ]
+                ],
+                submitted: false
             }
+        },
+        validations: {
+            name: { required },
+            birthDate: { required },
+            address: { required },
+            place: { required },
+            choice: { required },
+            date: { required }
         },
         // Allows any child component to `inject: ['provider']` and have access to it.
         provide() {
@@ -99,7 +120,6 @@
         mounted() {
             // Current date format for input type=date
             this.date = new Date().toISOString().slice(0, 10);
-            console.log(this.provider.context);
         },
         methods: {
             resetForm() {
@@ -108,9 +128,20 @@
                 this.address = '';
                 this.place = '';
                 this.choice = '';
-                this.date = new Date().toLocaleDateString();
+                this.date = new Date().toISOString().slice(0, 10);
+            },
+            submitForm() {
+                this.submitted = true;
+
+                this.$v.$touch();
+                if (this.$v.$invalid) {
+                    return;
+                } else {
+                    this.createPdf();
+                }
             },
             createPdf() {
+
                 let pdfName = 'Attestation_de_deplacement_derogatoire';
                 let doc = new jsPdf();
 
--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import '@/assets/css/tailwind.css'
 import '@/assets/css/style.css'
 import App from './App.vue'
 import Vue2TouchEvents from 'vue2-touch-events'
+import Vuelidate from "vuelidate/src";
 import './registerServiceWorker'
 
 Vue.use(Vue2TouchEvents)
+Vue.use(Vuelidate)
 
 Vue.config.productionTip = false
 
